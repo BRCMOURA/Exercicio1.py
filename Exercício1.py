76874ff013c4e6ee8b2d7fae2f236aa76b1669c9
BRCMOURA <brcmoura@gmail.com>
2024-10-04 20:38:27 -0300

    Add files via upload

---
+++ Exercício1.py
@@ -0,0 +1,8 @@
+#Soma de dois números
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segunto número: "))
+
+soma = (n1 + n2)
+
+print ("A soma dos dois números é:", soma)
